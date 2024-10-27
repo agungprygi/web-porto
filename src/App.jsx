dfc7dd70b69d464c611a6f8b4830fc6c84edc968
Thinkpad-T470 <agungprayogi305@gmail.com>
2024-10-27 19:51:24 +0700

    init: build the file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./section/Navbar.jsx";
+import Hero from "./section/Hero.jsx";
+
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+      <Hero />
+    </main>
+  )
+}
+export default App
