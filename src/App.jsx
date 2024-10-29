7e57fed63177ba0ac6760991f040cceaf6c25990
Agung Prayogi <agungprayogi305@gmail.com>
2024-10-29 16:24:18 +0700

    feat: Hero section and About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./section/Navbar.jsx";
 import Hero from "./section/Hero.jsx";
+import About from "./section/About.jsx";
 
 
 const App = () => {
   return (
-    <main className="max-w-7xl mx-auto">
+    <main className="max-w-screen-2xl mx-auto relative">
       <Navbar />
       <Hero />
+      <About />
     </main>
   )
 }
