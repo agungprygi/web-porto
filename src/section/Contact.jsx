fb33911e004d6eb2824efec8446719b5c8a14848
Agung Prayogi <agungprayogi305@gmail.com>
2024-11-14 16:40:26 +0700

    fix: route bug

--- src/section/Contact.jsx
+++ src/section/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
     }
   }
   return (
-    <section className="c-space my-20">
+    <section className="c-space my-20" id="contact">
       <div className="relative sm:min-h-screen flex items-center justify-center flex-col">
         <img src="/assets/terminal.png" alt="terminal" className="absolute inset-0 min-h-screen hidden sm:block"/>
         <div className="contact-container">
--- src/section/Experience.jsx
+++ src/section/Experience.jsx
@@ -8,7 +8,7 @@ import Developer from "../components/Developer.jsx";
 const Experience = () => {
   const [animationName, setAnimationName] = useState('idle');
   return (
-    <section className='c-space my-20'>
+    <section className='c-space my-20' id="work">
       <div className="w-full text-white-600">
         <h3 className="head-text">My Professional Experience</h3>
         <div className="work-container">
