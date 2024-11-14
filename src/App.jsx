d587f6c137eba02ba50add6ce9bfe446fd9bbf4b
Agung Prayogi <agungprayogi305@gmail.com>
2024-11-14 16:37:13 +0700

    feat: finalization porto

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from "./section/About.jsx";
 import Project from "./section/Project.jsx";
 import Client from "./section/Client.jsx";
 import Contact from "./section/Contact.jsx";
+import Footer from "./section/Footer.jsx";
+import Experience from "./section/Experience.jsx";
 
 
 const App = () => {
@@ -14,7 +16,9 @@ const App = () => {
       <About />
       <Project/>
       <Client/>
+      <Experience/>
       <Contact/>
+      <Footer/>
     </main>
   )
 }
